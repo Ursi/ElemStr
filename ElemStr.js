3e7995bbe09a71c848fc0b522c4d68379d1b0dfc
Mason Mackaman <masondeanm@aol.com>
2019-07-16 12:44:55 -0400

    Change all whitespace to tabs
    Unbeknownst to me, I was mixing up tabs and spaces because Atom made the
    spaces seem like tabs.

--- ElemStr.js
+++ ElemStr.js
@@ -1,80 +1,80 @@
 export default class ElemStr extends Array {
-    constructor(elem, str = '') {
-        super();
-        Object.defineProperties(this, {
-            fragment: {
-                value: document.createDocumentFragment(),
-            },
-            type: {
-                value: elem,
-            },
-        });
+	constructor(elem, str = '') {
+		super();
+		Object.defineProperties(this, {
+			fragment: {
+				value: document.createDocumentFragment(),
+			},
+			type: {
+				value: elem,
+			},
+		});
 
-        for (let char of str) this.append(char);
-    }
+		for (let char of str) this.append(char);
+	}
 
-    static get [Symbol.species]() {return Array;}
+	static get [Symbol.species]() {return Array;}
 
-    append(char, ...classNames) {
-        let elemChar = document.createElement(this.type);
-        elemChar.classList.add('elem-char', ...classNames);
-        elemChar.textContent = char;
-        elemChar.dataset.char = char;
-        this.push(elemChar);
-        this.fragment.appendChild(elemChar);
-        return elemChar;
-    }
+	append(char, ...classNames) {
+		let elemChar = document.createElement(this.type);
+		elemChar.classList.add('elem-char', ...classNames);
+		elemChar.textContent = char;
+		elemChar.dataset.char = char;
+		this.push(elemChar);
+		this.fragment.appendChild(elemChar);
+		return elemChar;
+	}
 
-    class(className) {
-        let classIndeces;
-        for (let i = 0; i < this.length; i++) {
-            if (this[i].classList.contains(className)) classIndeces.push(i);
-        }
+	class(className) {
+		let classIndeces;
+		for (let i = 0; i < this.length; i++) {
+			if (this[i].classList.contains(className)) classIndeces.push(i);
+		}
 
-        return classIndeces;
-    }
+		return classIndeces;
+	}
 
-    firstClass(className) {
-        for (let elem of this) {
-            if (elem.classList.contains(className)) return elem;
-        }
-    }
+	firstClass(className) {
+		for (let elem of this) {
+			if (elem.classList.contains(className)) return elem;
+		}
+	}
 
-    hasClass(className) {
-        return this.some(elem => elem.classList.contains(className));
-    }
+	hasClass(className) {
+		return this.some(elem => elem.classList.contains(className));
+	}
 
-    lastClass(className) {
-        let last
-        for (let elem of this) {
-            if (elem.classList.contains(className)) last = elem;
-        }
+	lastClass(className) {
+		let last
+		for (let elem of this) {
+			if (elem.classList.contains(className)) last = elem;
+		}
 
-        return last;
-    }
+		return last;
+	}
 
-    sort(func = undefined) {
-        if (func) {
-            Array.prototype.sort.call(this, (a, b) => func(a.textContent, b.textContent));
-        } else {
-            Array.prototype.sort.call(this, (a, b) => {
-                if (a.textContent === b.textContent) {
-                    return 0;
-                } else {
-                    return a.textContent < b.textContent ? -1 : 1
-                }
-            });
-        }
+	sort(func = undefined) {
+		if (func) {
+			Array.prototype.sort.call(this, (a, b) => func(a.textContent, b.textContent));
+		} else {
+			Array.prototype.sort.call(this, (a, b) => {
+				if (a.textContent === b.textContent) {
+					return 0;
+				} else {
+					return a.textContent < b.textContent ? -1 : 1
+				}
+			});
+		}
 
-        return this;
-    }
+		return this;
+	}
 
-    toString(){
-        let str = '';
-        for (let elem of this) {
-            str += elem.innerText;
-        }
+	toString(){
+		let str = '';
+		for (let elem of this) {
+			str += elem.innerText;
+		}
 
-        return str;
-    }
+		return str;
+	}
 };
